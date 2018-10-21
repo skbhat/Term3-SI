dcf0ef11c01f4bef1d9b44d4b501e96236c938d9
Srikrishna <skbhat@gmail.com>
2018-10-21 22:13:01 +0530

    Delete build files

--- ros/build/styx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srikbha/SDC/Mine/CarND-Capstone-master/ros/src/styx
-# Build directory: /home/srikbha/SDC/Mine/CarND-Capstone-master/ros/build/styx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
